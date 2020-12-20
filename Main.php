aae5947041dda37ff748a23b951aaacac12c78ba
Ben Werdmuller <ben@benwerd.com>
2020-12-19 16:38:23 -0800

    Using new-style IDs

--- Main.php
+++ Main.php
@@ -11,8 +11,8 @@
 
                 \Idno\Core\site()->addPageHandler('helloworld/?', 'IdnoPlugins\MyPlugin\Pages\HelloWorld', true);
                 \Idno\Core\site()->addPageHandler('/hello/edit/?', '\IdnoPlugins\MyPlugin\Pages\Edit');
-                \Idno\Core\site()->addPageHandler('/hello/edit/([A-Za-z0-9]+)/?', '\IdnoPlugins\MyPlugin\Pages\Edit');
-                \Idno\Core\site()->addPageHandler('/hello/delete/([A-Za-z0-9]+)/?', '\IdnoPlugins\MyPlugin\Pages\Delete');
+                \Idno\Core\site()->addPageHandler('/hello/edit/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\MyPlugin\Pages\Edit');
+                \Idno\Core\site()->addPageHandler('/hello/delete/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\MyPlugin\Pages\Delete');
 
                 \Idno\Core\site()->template()->extendTemplate('content/end', 'hello/end');
 
